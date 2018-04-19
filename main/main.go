6fc1f4ed24439f39bf719717898b188d9a53d460
sabyx <sabyx2006@yahoo.com>
2018-04-19 06:58:51 +0300

    Initial commit

---
+++ main/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+	"github.com/zserge/webview"
+	"fmt"
+	"net"
+	"log"
+)
+
+const PORT = 8765
+const WIDTH = 800
+const HEIGHT = 600
+const TITLE = "Webview Application"
+
+type writeHtml func(w http.ResponseWriter, r *http.Request)
+
+func main() {
+	url := startServer()
+	w := webview.New(webview.Settings{
+		Width:     WIDTH,
+		Height:    HEIGHT,
+		Title:     TITLE,
+		Resizable: false,
+		URL:       url,
+	})
+	w.SetColor(255, 255, 255, 255)
+	defer w.Exit()
+	w.Run()
+}
+
+func startServer() string {
+	ln, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", PORT))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go handle("/", index, ln)
+	go handle("/leo", leo, ln)
+
+	return "http://" + ln.Addr().String()
+}
+
+func handle(pattern string, function writeHtml, ln net.Listener) {
+	defer ln.Close()
+	http.HandleFunc(pattern, function)
+	log.Fatal(http.Serve(ln, nil))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Path
+	message = strings.TrimPrefix(message, "/")
+	message = "Hello " + message
+	message = message + "<a href='/leo'>Go To Leo</a>"
+	w.Write([]byte(message))
+}
+
+func leo(w http.ResponseWriter, r *http.Request) {
+	message := "Leo"
+	w.Write([]byte(message))
+}
